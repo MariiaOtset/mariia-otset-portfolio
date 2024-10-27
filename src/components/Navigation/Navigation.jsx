b09475f371b14c787989353becd48eaf4298fbec
MariiaOtset <152022954+MariiaOtset@users.noreply.github.com>
2024-10-27 12:39:32 +0000

    navigation for all devices done

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,6 +3,7 @@ import css from "./Navigation.module.css";
 import { RiCloseFill } from "react-icons/ri";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { GoArrowUpRight } from "react-icons/go";
+import { PiReadCvLogo } from "react-icons/pi";
 
 const Navigation = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -18,7 +19,7 @@ const Navigation = () => {
           My portfolio
         </a>
         <button className={css.buttons} type="button" onClick={toggleMenu}>
-          <RxHamburgerMenu className={css.icon} />
+          <RxHamburgerMenu className={`${css.icon} ${css.iconOpenMenu}`} />
         </button>
       </div>
       <div className={`${css.container} ${isMenuOpen ? "" : css.hidden}`}>
@@ -55,8 +56,15 @@ const Navigation = () => {
               Contacts <GoArrowUpRight className={css.arrow} />
             </a>
           </li>
-          <li className={css.linkWrapper}>
-            <a href="" className={css.link}></a>
+          <li className={css.cvLinkWrapper}>
+            <a
+              href="https://drive.google.com/file/d/1F9yYJmC0rAof0Y3eUA9_nUKEtrqgGjHo/view?usp=sharing"
+              className={`${css.link} ${css.cvWrapper}`}
+              target="_blank"
+            >
+              <p className={css.cv}>Download CV</p>
+              <PiReadCvLogo className={css.cvIcon} />
+            </a>
           </li>
         </ul>
       </div>
