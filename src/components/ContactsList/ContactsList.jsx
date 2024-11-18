452607d660b0522c71f1f95318c33098b7531470
MariiaOtset <152022954+MariiaOtset@users.noreply.github.com>
2024-11-18 16:51:46 +0000

    new contacts added

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -1,6 +1,8 @@
 import css from "./ContactsList.module.css";
 import { VscGithub } from "react-icons/vsc";
 import { FaLinkedin } from "react-icons/fa";
+import { IoLogoWhatsapp } from "react-icons/io5";
+import { BiLogoGmail } from "react-icons/bi";
 
 const ContactsList = () => {
   return (
@@ -16,7 +18,7 @@ const ContactsList = () => {
       </li>
       <li className={css.linkWrapper}>
         <a
-          href="www.linkedin.com/in/mariia-otset"
+          href="https://www.linkedin.com/in/mariia-otset/"
           target="_blank"
           className={css.link}
         >
@@ -24,14 +26,22 @@ const ContactsList = () => {
         </a>
       </li>
       <li className={css.linkWrapper}>
-        {/* <a href="" target="_blank" className={css.link}>
-            <VscGithub className={css.socialsIcon} />
-          </a> */}
+        <a
+          href="https://wa.me/qr/AH7BFQPMKJPKN1"
+          target="_blank"
+          className={css.link}
+        >
+          <IoLogoWhatsapp className={css.socialsIcon} />
+        </a>
       </li>
       <li className={css.linkWrapper}>
-        {/* <a href="" target="_blank" className={css.link}>
-            <VscGithub className={css.socialsIcon} />
-          </a> */}
+        <a
+          href="mailto:mariiaotset@gmail.com"
+          target="_blank"
+          className={css.link}
+        >
+          <BiLogoGmail className={css.socialsIcon} />
+        </a>
       </li>
     </ul>
   );
