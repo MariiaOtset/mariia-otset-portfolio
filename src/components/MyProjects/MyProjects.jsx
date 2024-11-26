51ca2fa443bd76d82bd51e67a2904e81af5e1d31
MariiaOtset <152022954+MariiaOtset@users.noreply.github.com>
2024-11-26 20:34:12 +0000

    width in skills section fixed

--- src/components/MyProjects/MyProjects.jsx
+++ src/components/MyProjects/MyProjects.jsx
@@ -6,12 +6,12 @@ const MyProjects = () => {
       <h2 className={css.sectionTitle}>My Projects</h2>
       <ul className={css.projectsList}>
         <li className={css.projectItem}>
-          <img
+          {/* <img
             src="../../images/project-1.png"
             alt="Game landing page"
             loading="lazy"
             className={css.projectImage}
-          />
+          /> */}
           <div></div>
         </li>
         <li className={css.projectItem}></li>
