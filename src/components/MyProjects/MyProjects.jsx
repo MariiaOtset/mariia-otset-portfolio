62be0d47d8150c05dbe3740be498f51ec6b05253
MariiaOtset <152022954+MariiaOtset@users.noreply.github.com>
2024-11-28 09:56:07 +0000

    styles for projects section added

--- src/components/MyProjects/MyProjects.jsx
+++ src/components/MyProjects/MyProjects.jsx
@@ -1,14 +1,25 @@
 import css from "./MyProjects.module.css";
 import ProjectItem from "../ProjectItem/ProjectItem.jsx";
 
+const projectsData = [
+  {
+    image: "/src/images/game-landing.jpg",
+    title: "Game Landing Page",
+    description: "An interactive landing page for a mobile match-3 game. I was responsible for developing the Gallery, How to Play, Game Video sections, as well as the Privacy Policy page. Commercial team project",
+    technologies: ["HTML", "CSS", "JavaScript"],
+    liveLink: "https://vitayusko.github.io/Puppy-Diary-Epic-Match-3/",
+    githubLink: "https://github.com/vitayusko/Puppy-Diary-Epic-Match-3"
+  },
+];
+
 const MyProjects = () => {
   return (
     <div className={css.projectsSectionWrapper}>
       <h2 className={css.sectionTitle}>My Projects</h2>
       <ul className={css.projectsList}>
-        {
-          <ProjectItem />
-       }
+        {projectsData.map((project, index) => (
+          <ProjectItem key={index} {...project}/>
+        ))}
       </ul>
     </div>
   );
--- src/components/ProjectItem/ProjectItem.jsx
+++ src/components/ProjectItem/ProjectItem.jsx
@@ -1,28 +1,34 @@
 import css from "./ProjectItem.module.css";
+import { LuLink } from "react-icons/lu";
+import { IoLogoGithub } from "react-icons/io5";
 
-const ProjectItem = () => {
+const ProjectItem = ({ image, title, description, technologies, liveLink, githubLink }) => {
   return (
     <div>
       <li className={css.projectItem}>
         <img
-          src="/src/images/game-landing.jpg"
-          alt="Game landing page"
+          src={image}
+          alt={title}
           loading="lazy"
           className={css.projectImage}
         />
         <div className={css.projectInfoContainer}>
-          <h3 className={css.projectTitle}>yftyfd</h3>
-          <p className={css.projectDescription}>zszsx</p>
+          <h3 className={css.projectTitle}>{title}</h3>
+          <p className={css.projectDescription}>{description}</p>
           <ul className={css.projectTechList}>
-            <li className={css.projectTechItems}>kko</li>
-            <li className={css.projectTechItems}>jbkj</li>
-            <li className={css.projectTechItems}>jbjk</li>
-            <li className={css.projectTechItems}>ljlk</li>
+            {technologies.map((tech, index) => (
+                <li key={index} className={css.projectTechItems}>{tech}</li>
+            ))}
           </ul>
 
-          <div>
-            <a href="" className={css.livePageLink}></a>
-            <a href="" className={css.githubLink}></a>
+          <div  className={css.linksWrapper}>
+            <a href={liveLink} className={css.pageLink}>
+            <LuLink className={css.icon}/>  
+            Live Page
+            </a>
+            <a href={githubLink} className={css.pageLink}>
+            <IoLogoGithub className={css.icon}/>
+            Github</a>
           </div>
         </div>
       </li>
