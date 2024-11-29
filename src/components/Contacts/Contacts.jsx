ecf7cd1eaf40fbf53b5bd6008aa9cba5c5b8a466
MariiaOtset <152022954+MariiaOtset@users.noreply.github.com>
2024-11-29 19:52:00 +0000

    styles for mobile version of contacts section done

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -10,27 +10,33 @@ const Contacts = () => {
   return (
     <div className={css.contactsSection}>
       <h2 className={css.contactsTitle}>Contacts</h2>
-      <ul className={css.contactsList}>
-        <li className={css.contactsItem}>
-          <BsTelephoneFill className={`${css.contactsIcon} ${css.phoneIcon}`} />
-          <a href="tell:+447391727924" className={css.contactsLink}>
-            +44 739 172 79 24
-          </a>
-        </li>
-        <li className={css.contactsItem}>
-          <HiOutlineMail className={`${css.contactsIcon} ${css.emailIcon}`} />
-          <a href="mailto:mariiaotset@gmail.com" className={css.contactsLink}>
-            mariiaotset@gmail.com
-          </a>
-        </li>
-        <li className={css.contactsItem}>
-          <IoLocationSharp
-            className={`${css.contactsIcon} ${css.locationIcon}`}
-          />
-          <p>Darlington, United Kingdom</p>
-        </li>
-      </ul>
-      <ContactsList />
+      <div className={css.contactsInfoWrapper}>
+        <ul className={css.contactsList}>
+          <li className={css.contactsItem}>
+            <BsTelephoneFill
+              className={`${css.contactsIcon} ${css.phoneIcon}`}
+            />
+            <a href="tell:+447391727924" className={css.contactsLink}>
+              +44 739 172 79 24
+            </a>
+          </li>
+          <li className={css.contactsItem}>
+            <HiOutlineMail className={`${css.contactsIcon} ${css.emailIcon}`} />
+            <a href="mailto:mariiaotset@gmail.com" className={css.contactsLink}>
+              mariiaotset@gmail.com
+            </a>
+          </li>
+          <li className={css.contactsItem}>
+            <IoLocationSharp
+              className={`${css.contactsIcon} ${css.locationIcon}`}
+            />
+            <p>Darlington, United Kingdom</p>
+          </li>
+        </ul>
+        <div className={css.contactsComponent}>
+          <ContactsList />
+        </div>
+      </div>
     </div>
   );
 };
