8f59338749147e381b7226219cf32cfd9d1ffc61
MariiaOtset <152022954+MariiaOtset@users.noreply.github.com>
2024-11-29 14:41:31 +0000

    contacts section started

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,10 +1,12 @@
-// import css from './Contacts.module.css';
+import css from "./Contacts.module.css";
 import ContactsList from "../ContactsList/ContactsList.jsx";
+// import { BsFillTelephoneFill } from "react-icons/bs";
 
 const Contacts = () => {
   return (
-    <div>
-      Contacts
+    <div className={css.contactsSection}>
+      <h2>Contacts</h2>
+
       <ContactsList />
     </div>
   );
